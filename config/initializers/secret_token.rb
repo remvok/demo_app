4266f44a189ae957c7652360159f0d20a96f3e46
void <xoid@list.ru>
2013-10-26 20:45:03 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4132be11ce303606c8f8c5dd198ba05e3f60b2db68b1f65a563290c02b4c60b0ced6a8ab39bece682a18a09eddb96137fb481bd4fa0195a7487aa865aad8899a'
